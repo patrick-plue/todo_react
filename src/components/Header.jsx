12283c8678d0b93a4bb8ab22f551f14e998bb095
Patrick Pluequett <ppluequett@gmail.com>
2022-03-16 20:44:41 +0100

    add basic buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function Header() {
-  return <div className="headerContainer">Header</div>;
+  // add react select tag
+  return (
+    <div className="headerContainer">
+      <button>Done</button>
+      <button>Delete</button>
+      <button>Archive</button>
+      <select>
+        <option>none</option>
+        <option>day</option>
+        <option>week</option>
+        <option>month</option>
+      </select>
+      <select>
+        <option>home</option>
+        <option>job</option>
+        <option>family</option>
+        <option>pet</option>
+      </select>
+    </div>
+  );
 }
 
 export default Header;
