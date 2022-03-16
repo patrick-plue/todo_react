b7b905a9490e9743a204d2caac11a5cf83ed1746
Patrick Pluequett <ppluequett@gmail.com>
2022-03-16 15:14:55 +0100

    delete unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import Header from './components/Header';
+import Item from './components/Item';
+import Sidebar from './components/Sidebar';
+import List from './components/List';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Todo React
+      <Header />
+      <Sidebar />
+      <List />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Item from './Item';
+
+function List() {
+  return <div>List</div>;
+}
+
+export default List;
