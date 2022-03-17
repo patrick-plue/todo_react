8725a79de61fd553e62d636dc059bf4a89e2f0ab
Patrick Pluequett <ppluequett@gmail.com>
2022-03-17 12:36:06 +0100

    prevent empty input

--- src/App.js
+++ src/App.js
@@ -20,17 +20,21 @@ function App() {
   //functions
   function addTask(event, text, progress, category) {
     event.preventDefault();
-    const newTask = {
-      text: text,
-      id: uuidv4(),
-      archived: false,
-      edit: false,
-      progress: progress,
-      category: category,
-      selected: false,
-      important: false,
-    };
-    setTasks([...tasks, newTask]);
+    if (text) {
+      const newTask = {
+        text: text,
+        id: uuidv4(),
+        archived: false,
+        edit: false,
+        progress: progress,
+        category: category,
+        selected: false,
+        important: false,
+      };
+      setTasks([...tasks, newTask]);
+    } else {
+      return;
+    }
   }
 
   function select(id) {
